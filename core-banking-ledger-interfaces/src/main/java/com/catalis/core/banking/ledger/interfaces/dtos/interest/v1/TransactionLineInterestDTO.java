e36c01d00b2075d13854d253f952d9f6396e07f0
Andrés Contreras Guillén <andres.contreras@getfirefly.io>
2025-05-02 12:05:24 +0200

    feat(interfaces): add transaction line DTOs
    - Add DTOs for deposit transactions
    - Add DTOs for fee transactions
    - Add DTOs for interest transactions
    - Add DTOs for transfer transactions
    - Add DTOs for withdrawal transactions

---
+++ core-banking-ledger-interfaces/src/main/java/com/catalis/core/banking/ledger/interfaces/dtos/interest/v1/TransactionLineInterestDTO.java
@@ -0,0 +1,50 @@
+package com.catalis.core.banking.ledger.interfaces.dtos.interest.v1;
+
+import com.catalis.core.banking.ledger.interfaces.dtos.BaseDTO;
+import com.catalis.core.utils.annotations.FilterableId;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * DTO representing detailed information about an interest transaction.
+ */
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class TransactionLineInterestDTO extends BaseDTO {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long transactionLineInterestId;
+
+    @FilterableId
+    private Long transactionId;
+
+    private String interestType;
+    private String interestDescription;
+    private String interestReference;
+    
+    @FilterableId
+    private Long interestRelatedAccountId;
+    
+    private String interestCalculationMethod;
+    private BigDecimal interestCalculationBase;
+    private BigDecimal interestRatePercentage;
+    private LocalDate interestAccrualStartDate;
+    private LocalDate interestAccrualEndDate;
+    private Integer interestDaysCalculated;
+    private String interestCurrency;
+    private BigDecimal interestTaxWithheldAmount;
+    private BigDecimal interestTaxWithheldRate;
+    private BigDecimal interestGrossAmount;
+    private BigDecimal interestNetAmount;
+    private LocalDateTime interestTimestamp;
+    private String interestProcessedBy;
+    private String interestSpanishTaxCode;
+}
